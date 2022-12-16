161bcadbc6138a093759aada9e81a64d6db19cec
Hutomo <hutomo@windowslive.com>
2022-12-16 05:48:35 +0100

    Add echo in generate_data

--- generate_data.sh
+++ generate_data.sh
@@ -1,2 +1,9 @@
-cd data/
-./dbgen -T L -s $1 -b dists.dss
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo "Missing scale factor as argument i.e. \"bash generate_data.sh 1\""
+else
+  cd data/
+  ./dbgen -T L -s $1 -b dists.dss
+fi
