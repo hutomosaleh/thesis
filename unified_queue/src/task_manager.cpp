c69b4daf32f7bfb095d3023d86a15d819daae181
Hutomo <hutomo@windowslive.com>
2022-12-21 22:14:08 +0100

    Fix time results

--- unified_queue/src/task_manager.cpp
+++ unified_queue/src/task_manager.cpp
@@ -12,15 +12,21 @@
 
 TaskManager::TaskManager(std::deque<Task> queue, int loops) : _loops(loops), _queue_size(queue.size()), _queue(queue) {};
 
+static double get_avg(int time, int rep)
+{
+  double time_ms = (double) time / 1e3;
+  return time_ms / rep;
+}
+
 void TaskManager::read_stats()
 {
   std::cout << "\n ==== Task Manager Stats ====" << std::endl;
   std::cout << "CPU Calls: " << _cpu_calls << std::endl;
-  std::cout << "CPU Total Time: " << _cpu_time << " ms" << std::endl;
+  std::cout << "CPU Total Time Avg: " << get_avg(_cpu_time, _loops) << " ms" << std::endl;
   std::cout << "GPU Calls: " << _gpu_calls << std::endl;
-  std::cout << "GPU Total Time: " << _gpu_time << " ms" << std::endl;
-  std::cout << "Total Time: " << _gpu_time + _cpu_time << " ms" << std::endl;
-  std::cout << "Total Time per loop avg: " << (double)(_gpu_time + _cpu_time) / _loops << " ms" << std::endl;
+  std::cout << "GPU Total Time Avg: " << get_avg(_gpu_time, _loops) << " ms" << std::endl;
+  std::cout << "Total Time: " << (double)(_gpu_time + _cpu_time) / 1e3 << " ms" << std::endl;
+  std::cout << "Total Time per loop avg: " << get_avg(_gpu_time+_cpu_time, _loops) << " ms" << std::endl;
   std::cout << "Result: " << std::fixed << _result << std::endl;
   std::cout << "Hits: " << _hits << std::endl;
   std::cout << "Total tuples: " << TASK_SIZE*(_cpu_calls+_gpu_calls) << std::endl;
@@ -48,14 +54,14 @@ void TaskManager::start_hybrid_consumer()
     _gpu_calls++;
     auto start_gpu = std::chrono::steady_clock::now();
     task_gpu.consume(GPU_TASK);
-    _gpu_time += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_gpu).count();
+    _gpu_time += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start_gpu).count();
     _hits += task_gpu.get_hits();
     for (double r = _result; !_result.compare_exchange_weak(r, r+task_gpu.get_result()););
     if (!_pop_task(task)) break;
     _cpu_calls++;
     auto start = std::chrono::steady_clock::now(); 
     task.consume(CPU_TASK);
-    _cpu_time += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+    _cpu_time += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
     _hits += task.get_hits();
     for (double r = _result; !_result.compare_exchange_weak(r, r+task.get_result()););
   }
@@ -70,7 +76,7 @@ void TaskManager::start_device_consumer()
     _gpu_calls++;
     auto start = std::chrono::steady_clock::now();
     task.consume(GPU_TASK);
-    _gpu_time += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+    _gpu_time += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
     _hits += task.get_hits();
     for (double r = _result; !_result.compare_exchange_weak(r, r+task.get_result()););
   }
@@ -85,7 +91,7 @@ void TaskManager::start_host_consumer()
     _cpu_calls++;
     auto start = std::chrono::steady_clock::now();
     task.consume(CPU_TASK);
-    _cpu_time += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+    _cpu_time += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
     _hits += task.get_hits();
     for (double r = _result; !_result.compare_exchange_weak(r, r+task.get_result()););
   }
