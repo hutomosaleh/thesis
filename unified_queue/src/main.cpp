810166af32588442868aaf90beccc030d7542c84
Hutomo <hutomo@windowslive.com>
2022-12-22 21:03:52 +0100

    Add task size as launch argument

--- unified_queue/src/main.cpp
+++ unified_queue/src/main.cpp
@@ -12,10 +12,10 @@ int main(int argc, char** argv)
   int type = (argc > 1) ? atoi(argv[1]) : 2;
   bool overwrite_file = (argc > 2 && std::string(argv[2]) == "overwrite") ? true : false;
   int loop_count = (argc > 3 && atoi(argv[3]) != 0) ? atoi(argv[3]) : 1;
-  std::cout << "Type: " << type << " | Loop count: " << loop_count << std::endl;
+  int task_size = (argc > 4) ? atoi(argv[4]) : TASK_SIZE;
+  std::cout << "Type: " << type << " | Loop count: " << loop_count << " | Task size: " << task_size << std::endl;
   std::cout << "0 : CPU | 1: GPU | Else: Hybrid" << std::endl;
   
-  int task_size = TASK_SIZE;
 
   // Generate tasks
   TaskGenerator task(task_size);
